accbe4ad8410e13af2480712c6e24ea82e1a01aa
Maria Trujillo <mptruj015@gmail.com>
2020-09-04 21:31:53 +0200

    finished oop file, reached mvp

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,16 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
 
+  def __str__(self):
+    return f"{self.name}, {self.lat}, {self.lon}"
+
+  def __repr__(self):
+    return f"{self.name}, {self.lat}, {self.lon}"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -16,13 +26,23 @@
 # should not be loaded into a City object.
 cities = []
 
+import csv
+
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # Ensure that the lat and lon valuse are all floats
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+
+  with open('cities.csv', newline = '') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    next(csv_reader)
+
+    for row in csv_reader:
+      new_city = City(row[0], row[3], row[4])
+      cities.append(new_city)
     
-    return cities
+  return cities
 
 cityreader(cities)
 
--- src/comp/comp.py
+++ src/comp/comp.py
@@ -66,18 +66,28 @@ print(e)
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [f"({i.name},{i.age})" for i in humans if i.age <= 32 and i.age >= 27]
+
+# for i in humans:
+#     if i.age <= 32 and i.age >= 27:
+#         print(f"{i.name},{i.age}")
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+g = [Human(i.name.upper(), i.age + 5) for i in humans]
+
+# for i in humans:
+#     print(f"{i.name.upper()}, {i.age + 5} ")
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = []
+h = [math.sqrt(i.age) for i in humans]
+
+# for i in humans:
+#     print(f"{math.sqrt(i.age)}")
 print(h)
